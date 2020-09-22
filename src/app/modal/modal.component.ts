8fec096e6cadad9762df5e0ff89a357579703c65
RaviganeshR <raveeganesh@gmail.com>
2020-09-22 12:32:21 +0530

    Redesigned chart modal table data

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -16,20 +16,21 @@ interface DialogData {
   encapsulation: ViewEncapsulation.None,
 })
 export class ModalComponent implements OnInit {
-  displayedColumns: string[] = ['Rep Information', 'Department', 'Sign-In Location', 'Sign-In By'];
+  displayedColumns: string[] = ['Sign-In','Sign-Out','Event-Type', 'Department', 'Sign-In-Location', 'Sign-Out-Location','Purpose-of-visit','Override-Guest-Pass-Reason','Badge-Printed'];
   selectedCar = 'Success';
    ELEMENTDATA = [
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Accounting', sighInLoc: 'Avon', signInBy: 'amala@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'EP Lab', sighInLoc: 'Avon', signInBy: 'ravi@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Administration', sighInLoc: 'Avon', signInBy: 'subburaj@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Administration', sighInLoc: 'Avon', signInBy: 'saravanan@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Administration', sighInLoc: 'Avon', signInBy: 'pandi@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'EP Lab', sighInLoc: 'Avon', signInBy: 'vasanth@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Accounting', sighInLoc: 'Avon', signInBy: 'venkat@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Accounting', sighInLoc: 'Avon', signInBy: 'poormnima@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Administration', sighInLoc: 'Avon', signInBy: 'venkat@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'EP Lab', sighInLoc: 'Avon', signInBy: 'ravi@mookambikainfo.com'},
-    {'RepInformation': 'AmalaPandian@mookambikainfo.com 80822', Department: 'Administration', sighInLoc: 'Avon', signInBy: 'ravi@mookambikainfo.com'},
+    {signIn: 'Aug 29, 2020 01:02 AM', signOut:'Aug 29, 2020 04:45 PM', Department: 'Accounting', sighInLoc: 'Apollo-chennai', signOutLoc: 'Apollo-chennai', eventType:'Printed on site', signInBy: 'amala@mookambikainfo.com',poVisit:'Long Appt Test',guestPassReason:'Long Appt Test',badgePrinted:'true'},
+    {signIn: 'Aug 28, 2020 07:40 AM', signOut:'Aug 28, 2020 04:45 PM', Department: 'Administration', sighInLoc: 'KMC-chennai', signOutLoc: 'KMC-chennai', eventType:'Print Remotely', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 25, 2020 05:56 PM', signOut:'Aug 25, 2020 06:59 PM', Department: 'Cath Lab', sighInLoc: 'Global-chennai', signOutLoc: 'Global-chennai', eventType:'Guest pass', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 29, 2020 01:02 AM', signOut:'Aug 29, 2020 04:45 PM', Department: 'Accounting', sighInLoc: 'Apollo-chennai', signOutLoc: 'Apollo-chennai', eventType:'Printed on site', signInBy: 'amala@mookambikainfo.com',poVisit:'Long Appt Test',guestPassReason:'Long Appt Test',badgePrinted:'true'},
+    {signIn: 'Aug 28, 2020 07:40 AM', signOut:'Aug 28, 2020 04:45 PM', Department: 'Administration', sighInLoc: 'KMC-chennai', signOutLoc: 'KMC-chennai', eventType:'Print Remotely', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 25, 2020 05:56 PM', signOut:'Aug 25, 2020 06:59 PM', Department: 'Cath Lab', sighInLoc: 'Global-chennai', signOutLoc: 'Global-chennai', eventType:'Guest pass', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 29, 2020 01:02 AM', signOut:'Aug 29, 2020 04:45 PM', Department: 'Accounting', sighInLoc: 'Apollo-chennai', signOutLoc: 'Apollo-chennai', eventType:'Printed on site', signInBy: 'amala@mookambikainfo.com',poVisit:'Long Appt Test',guestPassReason:'Long Appt Test',badgePrinted:'true'},
+    {signIn: 'Aug 28, 2020 07:40 AM', signOut:'Aug 28, 2020 04:45 PM', Department: 'Administration', sighInLoc: 'KMC-chennai', signOutLoc: 'KMC-chennai', eventType:'Print Remotely', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 25, 2020 05:56 PM', signOut:'Aug 25, 2020 06:59 PM', Department: 'Cath Lab', sighInLoc: 'Global-chennai', signOutLoc: 'Global-chennai', eventType:'Guest pass', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 29, 2020 01:02 AM', signOut:'Aug 29, 2020 04:45 PM', Department: 'Accounting', sighInLoc: 'Apollo-chennai', signOutLoc: 'Apollo-chennai', eventType:'Printed on site', signInBy: 'amala@mookambikainfo.com',poVisit:'Long Appt Test',guestPassReason:'Long Appt Test',badgePrinted:'true'},
+    {signIn: 'Aug 28, 2020 07:40 AM', signOut:'Aug 28, 2020 04:45 PM', Department: 'Administration', sighInLoc: 'KMC-chennai', signOutLoc: 'KMC-chennai', eventType:'Print Remotely', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
+    {signIn: 'Aug 25, 2020 05:56 PM', signOut:'Aug 25, 2020 06:59 PM', Department: 'Cath Lab', sighInLoc: 'Global-chennai', signOutLoc: 'Global-chennai', eventType:'Guest pass', signInBy: 'amala@mookambikainfo.com',poVisit:'Test',guestPassReason:'Test',badgePrinted:'false'},
   ];
   cars = [
     {value: 'Success', viewValue: 'Success'},
